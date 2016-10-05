851d2696821e3a2db735da7eb56a8de478361b57
coco <308695699@qq.com>
2016-10-05 18:32:55 +0800

    iswiper update

---
+++ README.md
@@ -0,0 +1,28 @@
+##iswiper -- 移动端滑动插件
+
+小巧的移动端滑动插件，支持无限轮播滚动
+
+```html
+<div class="swiper">
+    <div class="item">
+        <img src="xxx1.jpg">
+    </div>
+    <div class="item">
+        <img src="xxx2.jpg">
+    </div>
+    <div class="item">
+        <img src="xxx3.jpg">
+    </div>
+    <div class="item">
+        <img src="xxx4.jpg">
+    </div>
+</div>
+<script src="../swiper.js"></script>
+<script>
+    var swpier = new Swiper();
+</script>
+```
+
+##License
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var header = require('gulp-header');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var tap = require('gulp-tap');
+var browserSync = require('browser-sync');
+var pkg = require('./package.json');
+
+gulp.task('build', function () {
+    gulp.src('src/example/**/*')
+        .pipe(gulp.dest('dist/example'));
+
+    gulp.src('src/iswiper.less')
+        .pipe(less().on('error', function (e){
+            console.error(e.message);
+            this.emit('end');
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist'))
+        .pipe(minify())
+        .pipe(rename(function (file){
+            file.basename += '.min';
+        }))
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('src/iswiper.js')
+        .pipe(tap(function(file, t){
+            var contents = file.contents.toString();
+            contents = contents.replace('${version}', pkg.version);
+            file.contents = new Buffer(contents);
+        }))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename(function (file){
+            file.basename += '.min';
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        ui: {
+            port: 8081,
+            weinre: {
+                port: 9090
+            }
+        },
+        port: 8080,
+        startPath: '/example'
+    });
+});
+
+gulp.task('watch', ['build', 'server'], function (){
+    gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('default', ['watch']);
